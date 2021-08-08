1c6d72b34c2bb622cde5f58594b92ac58517832a
yakipurin-abc <k.nishida0425@gmail.com>
2021-08-08 12:03:11 +0900

    first

---
+++ database/factories/ExerciseFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Exercise;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ExerciseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Exercise::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'profile' => Str::random(10),
+        ];
+    }
+}
